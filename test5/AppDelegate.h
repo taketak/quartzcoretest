9b3c51f5ef7e5ddc21058ba9053d8ccab7b3b00d
kunren-n1 <kunren-n1@MacBook13.local>
2015-04-02 20:01:45 +0900

    Initial Commit

---
+++ test5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test5
+//
+//  Created by kunren-n1 on 2015/04/02.
+//  Copyright (c) 2015年 TakeuchiSyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test5
+//
+//  Created by kunren-n1 on 2015/04/02.
+//  Copyright (c) 2015年 TakeuchiSyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
